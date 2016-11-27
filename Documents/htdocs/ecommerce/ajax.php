8d41a4f7a953f9520a6d7de3a79f11b31a3bba32
Jean David Olekhnovitch <jdo@scopika.com>
2016-11-27 12:45:15 +0100

    Premiere version

---
+++ Documents/htdocs/ecommerce/ajax.php
@@ -0,0 +1,47 @@
+<div class="row">
+  <div class="col-md-4">
+    <div id="demo">
+<ul>
+  <li class="first dragend-page"></li>
+  <li class="dragend-page"></li>
+  <li class="middle dragend-page"></li>
+  <li class="dragend-page"></li>
+  <li class="last dragend-page"></li>
+</ul>
+</div>
+    </div>
+    <div class="col-md-4"></div>
+    <div class="col-md-4"></div>
+</div>
+
+
+<div class="row">
+  <div class="col-md-4"></div>
+<div class="col-md-4"><div id="typologie">
+<ul>
+  <li class="first dragend-page"><h3>Bordeaux</h3></li>
+  <li class="dragend-page"><h3>Bourgogne</h3></li>
+  <li class="middle dragend-page"><h3>Languedoc</h3></li>
+  <li class="dragend-page"><h3>Alsace</h3></li>
+  <li class="last dragend-page"><h3>Pays de Loire</h3></li>
+</ul>
+</div>
+</div>
+<div class="col-md-4"></div>
+</div>
+
+
+<div class="row">
+<div class="col-md-4"></div>
+<div class="col-md-4"></div>
+<div class="col-md-4">  <div id="bouteilles">
+<ul>
+  <li class="first dragend-page"><img src="img/1.jpg" /></li>
+  <li class="dragend-page"><img src="img/2.jpg" /></li>
+  <li class="middle dragend-page"><img src="img/3.jpg" /></li>
+  <li class="dragend-page"><img src="img/4.jpg" /></li>
+  <li class="last dragend-page"><img src="img/5.jpg" /></li>
+</ul>
+</div>
+</div>
+</div>
