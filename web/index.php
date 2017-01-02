1317182a71398b294beab6c71a20d0172244324d
florentingarnier <garnier.florentin@gmail.com>
2017-01-02 23:10:28 +0100

    amélioration du style

--- web/index.php
+++ web/index.php
@@ -1,56 +1,151 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="fr">
 <head>
-	<title>Yooway</title>
-	<link rel="stylesheet" type="text/css" href="js/jquery-ui/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yooway</title>
+    <link rel="stylesheet" type="text/css" href="js/jquery-ui/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <div class="container">
 
-	<div id="row-1" class="row">
-		<div id="til-0" class="til">
+
+    <div id="row-1" class="row">
+        <div id="til-0" class="til">
             <img src="img/images-1.jpg" alt="">
             <div class="price">
-                21,00 €
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
             </div>
         </div>
-		<div id="til-1" class="til">
+        <div id="til-1" class="til">
             <img src="img/images-2.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
         </div>
-		<div id="til-2" class="til">
+        <div id="til-2" class="til">
             <img src="img/images-3.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
         </div>
-	</div>
+    </div>
 
 
-	<div id="row-2" class="row">
-		<div id="til-3" class="til">
+    <div id="row-2" class="row">
+        <div id="til-3" class="til">
             <img src="img/images.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
         </div>
-		<div id="til-4" class="til">
+        <div id="til-4" class="til">
             <img src="img/imgres-1.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
         </div>
-		<div id="til-5" class="til">
-            <img src="img/imgres-2.jpg" alt="">
-        </div>
-	</div>
 
-	<div id="row-3" class="row">
-		<div id="til-6" class="til">
-            <img src="img/imgres.jpg" alt="">
+        <div id="til-5" class="til">
+            <img src="img/imgres-2.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
         </div>
-		<div id="til-7" class="til yooway-question">
+    </div>
+
+    <div id="row-3" class="row">
+        <div id="til-6" class="til">
+            <img src="img/imgres.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
+        </div>
+        <div id="til-7" class="til yooway-question">
             <p>Vin à boire maintenant?</p>
-            <div class="reponse-oui">
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
                 Oui
             </div>
-            <div class="reponse-non">
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
                 Non
             </div>
         </div>
-		<div id="til-8" class="til"><img src="img/images-1.jpg" alt=""></div>
-	</div>
+        <div id="til-8" class="til">
+            <img src="img/images-1.jpg" alt="">
+            <div class="price">
+                <p>21,00 €</p>
+            </div>
+            <div class="answer like">
+                <img src="img/left.png" alt="to the left">
+                J'aime
+            </div>
+            <div class="answer dislike">
+                <img src="img/right.png" alt="to the right">
+                Je n'aime pas
+            </div>
+        </div>
+    </div>
 </div>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/jquery-ui/jquery-ui.js"></script>
